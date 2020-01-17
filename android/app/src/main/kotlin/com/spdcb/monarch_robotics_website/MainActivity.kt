9c6f1d38df6e81cb0a4adb1d52419947dcd72130
jkmosa01 <35510468+jkmosa01@users.noreply.github.com>
2020-01-16 18:41:39 -0700

    first commit

---
+++ android/app/src/main/kotlin/com/spdcb/monarch_robotics_website/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.spdcb.monarch_robotics_website
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
